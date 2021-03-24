047c6e8b850dc346ea7c9141b9926994bd97edc0
maryammanzoor <70525413+maryammanzoor@users.noreply.github.com>
2021-03-24 23:27:36 +0500

    Update binToFloat_provided.c

--- pa3/binToFloat/binToFloat_provided.c
+++ pa3/binToFloat/binToFloat_provided.c
@@ -3,41 +3,74 @@
 #include <stdio.h>
 #include <math.h>
 
-#define EXP_SZ 8
-#define FRAC_SZ 23
 
-int main(int argc, char *argv[]) {
-
-    FILE* fp = fopen(argv[1], "r");
-    if (!fp) {
-        perror("fopen failed");
-        return EXIT_FAILURE;
-    }
-
-    // SETUP
-
-    // first, read the binary number representation of float point number
-    char buff;
-    unsigned int binary = 0;
-    for (int i=EXP_SZ+FRAC_SZ; 0<=i; i--) { // read MSB first as that is what comes first in the file
-        /* ... */
-    }
-
-    // float number = *(float *)&binary; // you are not allowed to do this.
-
-    /* ... */
-
-    // E
-    /* ... */
-
-    // M
-    /* ... */
-
-    // https://www.tutorialspoint.com/c_standard_library/c_function_ldexp.htm
-    double number = ldexp ( m, e );
-    number = sign ? -number : number;
-    printf("%e\n", number);
-
-    return EXIT_SUCCESS;
-
+int power(int base, int exponent){
+	int result=1;
+	for(exponent; exponent>0; exponent--){
+		result = result * base;
+	}
+	return result;
 }
+
+typedef union { 
+
+        float f; 
+        struct
+        { 
+
+
+                unsigned int m : 23; 
+                unsigned int e : 8; 
+                unsigned int sign : 1; 
+
+        } stu; 
+} myfloat; 
+
+
+unsigned int convertToInt(int* arr, int low, int high) 
+{ 
+        unsigned f = 0, i; 
+        for (i = high; i >= low; i--) { 
+                f = f + arr[i] * power(2, high - i); 
+        } 
+        return f; 
+} 
+
+
+int main(int argc, char *argv[]) 
+{ 
+
+	    FILE* fp = fopen(argv[1], "r");
+    	if (!fp) {
+        	perror("fopen failed");
+        	return EXIT_FAILURE;
+    	}
+        // Get the 32-bit floating point number 
+        unsigned int input[32] ;
+        int i ;
+        
+        for(i = 0 ; i<32 ;i++){
+        	input[i]=((char)fgetc(fp)-'0');
+                // scanf("%d",&input[i]);
+        }
+
+        myfloat var; 
+
+
+        unsigned f = convertToInt(input, 9, 31); 
+
+        var.stu.m = f; 
+
+
+        f = convertToInt(input, 1, 8); 
+
+ 
+        var.stu.e = f; 
+
+
+        var.stu.sign = input[0]; 
+
+        printf("The float value of the given"
+                " IEEE-754 representation is : \n"); 
+        printf("%f", var.f); 
+} 
